97f1f74a7f59ce26bcb203edb3781fb0d16ddcdd
yuanchaoxin <lboy_2015@163.com>
2022-03-23 20:46:00 +0800

    init

---
+++ src/main/java/com/atguigu/demojenkins/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.atguigu.demojenkins.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @GetMapping
+    public String findAll() {
+        return "hello";
+    }
+}
